7f2d6811e8503553eff1d779e65dbbb98681b0e2
Eritsuu96 <133488662+Eritsuu@users.noreply.github.com>
2023-10-20 20:08:40 +0700

    Update yt_dlp_download.py

--- bot/helper/mirror_utils/download_utils/yt_dlp_download.py
+++ bot/helper/mirror_utils/download_utils/yt_dlp_download.py
@@ -11,7 +11,7 @@ from ..status_utils.yt_dlp_download_status import YtDlpDownloadStatus
 from bot.helper.mirror_utils.status_utils.queue_status import QueueStatus
 from bot.helper.ext_utils.bot_utils import sync_to_async, async_to_sync
 from bot.helper.ext_utils.task_manager import is_queued
-from bot.helper.ext_utils.atrocious_utils import check_filename, limit_checker, stop_duplicate_check
+from bot.helper.ext_utils.aya_utils import check_filename, limit_checker, stop_duplicate_check
 
 LOGGER = getLogger(__name__)
 
