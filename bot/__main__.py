01c8badce824e8b9224efe653d7ced6dbb2747b3
Eritsuu96 <133488662+Eritsuu@users.noreply.github.com>
2023-10-21 09:58:46 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -22,7 +22,7 @@ from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.button_build import ButtonMaker
 from bot.helper.listeners.aria2_listener import start_aria2_listener
 from .modules import authorize, broadcast, clone, gd_count, gd_delete, cancel_mirror, gd_search, mirror_leech, photo_upload, status, torrent_search, torrent_select, ytdlp, rss, shell, eval, users_settings, bot_settings
-from .core import bypasser, freewall
+from bot.core import bypasser, freewall
 
 async def stats(client, message):
     if await aiopath.exists('.git'):
