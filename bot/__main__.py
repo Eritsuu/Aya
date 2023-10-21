3ec90004fb5b1078f2e8e7c3bf048b262b3e5830
Eritsuu96 <133488662+Eritsuu@users.noreply.github.com>
2023-10-21 10:45:13 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -152,7 +152,6 @@ NOTE: Try each command without any argument to see more detalis.
 /{BotCommands.ExecCommand}: Run Commands In Exec (Only Owner).
 /{BotCommands.ClearLocalsCommand}: Clear {BotCommands.EvalCommand} or {BotCommands.ExecCommand} locals (Only Owner).
 /{BotCommands.RssCommand}: RSS Menu.
-/{BotCommands.BypassCommand}: Bypass Link.
 '''
 
 
